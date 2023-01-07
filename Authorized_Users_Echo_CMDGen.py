1581e3e8ace3d72c781034000b76b58509601b1c
GuitarGuy99 <122157280+GuitarGuy99@users.noreply.github.com>
2023-01-07 12:19:06 +0000

    Create Authorized_Users_Echo_CMDGen.py

---
+++ Authorized_Users_Echo_CMDGen.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import os
+
+print("As long as your public key is saved as 'id_rsa.pub' this will work!")
+
+
+filepath = os.getenv('HOME') + "/.ssh/id_rsa.pub"
+rsa = open(filepath,"r")
+user = input("What is the user we have to ssh into?")
+
+if user == 'root':
+    print(f"\n \necho '{rsa.read()}' > /{user}/.ssh/authorized_keys")
+
+else:
+    print(f"\n \necho '{rsa.read()}' > /home/{user}/.ssh/authorized_keys")
